601d38537c930c36b61fe55608c17cc087291b4d
Sridar-cod <sridar9771@gmail.com>
2025-01-02 08:26:51 +0530

    twoSum

--- array_dsa.js
+++ array_dsa.js
@@ -1,19 +1,19 @@
 function linearSearch(arr, target) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === target) {
-            return i; 
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
     }
-    return -1; 
+  }
+  return -1;
 }
-
 
 const arr = [10, 20, 30, 40, 50];
 const target = 30;
 
 const result = linearSearch(arr, target);
 if (result !== -1) {
-    console.log(`Element ${target} found at index ${result}.`);
+  console.log(`Element ${target} found at index ${result}.`);
 } else {
-    console.log(`Element ${target} not found.`);
+  console.log(`Element ${target} not found.`);
 }
+console.log("11323")
