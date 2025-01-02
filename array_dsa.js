646440bcff74f53281f579c5e25d3eadcab4d8b4
sridar14 <sridar1404@gmail.com>
2025-01-02 08:08:16 +0530

    initial file insert

---
+++ array_dsa.js
@@ -0,0 +1,19 @@
+function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+
+const arr = [10, 20, 30, 40, 50];
+const target = 30;
+
+const result = linearSearch(arr, target);
+if (result !== -1) {
+    console.log(`Element ${target} found at index ${result}.`);
+} else {
+    console.log(`Element ${target} not found.`);
+}
